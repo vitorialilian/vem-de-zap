66f685ac1eac1faa7d6ce215efb50046b88eb5e0
lilyv30 <127898471+vitorialilian@users.noreply.github.com>
2023-07-18 20:59:14 -0300

    Update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
-const mensagens = document.querySelectorAll('div');
+// function mandaZap(elemento) {
+    
+    
+//     const mensagens = document.querySelectorAll('div');
+    
+//     mensagens.addEventListener('click', () => {
+//         let texto = elemento.firstElementChild.innerText;
+//         let numero = prompt("Qual é o número?");
+//     })
+// }
+// mandaZap()
 
-mensagens.addEventListener('click', () => {
-    let texto = elemento.firstElementChild.innerText;
-    let numero = prompt("Qual é o número?");
-})
+
+const mensagens = document.querySelectorAll('div');
+    
+    mensagens.addEventListener('click', () => {
+        mandaZap()
+    })
+
+    function mandaZap() {
+        let texto = elemento.firstElementChild.innerText;
+        let numero = prompt("Qual é o número?");
+    }
+
