ba3c9130325aa77a6d64b10a68f027fd45a61da9
lilyv30 <127898471+vitorialilian@users.noreply.github.com>
2023-07-18 20:05:55 -0300

    Update

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const mensagens = document.querySelectorAll('div');
+
+mensagens.addEventListener('click', () => {
+    let texto = elemento.firstElementChild.innerText;
+    let numero = prompt("Qual é o número?");
+})
