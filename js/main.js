6c5252d823973a80d4a122b55942ce228a15fe1d
lilyv30 <127898471+vitorialilian@users.noreply.github.com>
2023-07-19 21:06:49 -0300

    Resolução

--- js/main.js
+++ js/main.js
@@ -1,23 +1,11 @@
-// function mandaZap(elemento) {
+const mensagens = document.querySelectorAll('div')
 
-
-//     const mensagens = document.querySelectorAll('div');
-
-//     mensagens.addEventListener('click', () => {
-//         let texto = elemento.firstElementChild.innerText;
-//         let numero = prompt("Qual é o número?");
-//     })
-// }
-// mandaZap()
-
-
-const mensagens = document.querySelector('div')
-
-mensagens.addEventListener('click', () => {
-    let texto = mensagens.firstElementChild.innerText;
-    let numero = prompt("Qual é o número?");
-    let zapLink = `https://api.whatsapp.com/send?phone=55${numero}&text=${texto}`;
-    window.open(zapLink);
-})
-
+mensagens.forEach((div) => {
+    div.onclick = () => {
+        let texto = div.innerText;
+        let numero = prompt("Qual é o número?");
+        let zapLink = `https://api.whatsapp.com/send?phone=55${numero}&text=${texto}`;
+        window.open(zapLink);
+    }
+});
 
